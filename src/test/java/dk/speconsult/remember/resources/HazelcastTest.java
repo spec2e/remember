4140a8204014b29cac69244f4abd0add02503551
soeren.pedersen <svx@topdanmark.dk>
2013-12-11 09:18:31 +0100

    update

---
+++ src/test/java/dk/speconsult/remember/resources/HazelcastTest.java
@@ -0,0 +1,11 @@
+package dk.speconsult.remember.resources;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: svx
+ * Date: 10-12-13
+ * Time: 16:20
+ * To change this template use File | Settings | File Templates.
+ */
+public class HazelcastTest {
+}
